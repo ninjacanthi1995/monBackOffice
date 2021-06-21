326dc6f26b69b661f75a4badf13eb337bee70626
Chau HOANG <hoangminhchaucp54@gmail.com>
2021-06-21 20:44:22 +0200

    Fix bugs

--- routes/index.js
+++ routes/index.js
@@ -11,23 +11,34 @@ router.get('/', function(req, res) {
 });
 
 router.post('/add-contact', async (req, res) => {
-  // message = 'Contact added';
-  const newContact = new contactModel({
-    lastname: req.body.lastname,
-    firstname: req.body.firstname,
-    email: req.body.email,
-    age: req.body.age,
-    contactType: req.body.contactType,
+  const searchContact = await contactModel.findOne({
+    email: req.body.email
   });
-  const savedContact = await newContact.save();
-
-  res.render('index', { message: 'Contact added' });
+  console.log(searchContact);
+  if (!searchContact) {
+    const newContact = new contactModel({
+      lastname: req.body.lastname,
+      firstname: req.body.firstname,
+      email: req.body.email,
+      age: req.body.age,
+      contactType: req.body.contactType,
+    });
+    const savedContact = await newContact.save();
+    res.render('index', { message: 'Contact added' });
+  } else {
+    res.redirect('/');
+  }
 });
 
 router.get('/contacts', async (req, res) => {
   const contacts = await contactModel.find();
 
-  res.render('contacts', { contacts });
+  res.render('contacts', { 
+    contacts, 
+    contactType: 'famille',
+    ageMin: 0,
+    ageMax: 120  
+  });
 });
 
 router.get('/edit-page', async (req, res) => {
@@ -37,23 +48,40 @@ router.get('/edit-page', async (req, res) => {
 });
 
 router.post('/edit-contact', async (req, res) => {
-  await contactModel.updateOne({ email: req.body.oldEmail }, {
-    lastname: req.body.lastname,
-    firstname: req.body.firstname,
-    email: req.body.email,
-    age: req.body.age,
-    contactType: req.body.contactType,
+  const searchContact = await contactModel.findOne({
+    email: req.body.email
   });
-  const contacts = await contactModel.find();
-
-  res.render('contacts', { contacts });
+  if (!searchContact) {
+    await contactModel.updateOne({ email: req.body.oldEmail }, {
+      lastname: req.body.lastname,
+      firstname: req.body.firstname,
+      email: req.body.email,
+      age: req.body.age,
+      contactType: req.body.contactType
+    });
+    const contacts = await contactModel.find();
+  
+    res.render('contacts', { 
+      contacts, 
+      contactType: 'famille',
+      ageMin: 0,
+      ageMax: 120  
+    });
+  } else {
+    res.redirect('/contacts');
+  }
 });
 
 router.get('/delete-contact', async (req, res) => {
   await contactModel.deleteOne({ email: req.query.email });
   const contacts = await contactModel.find();
 
-  res.render('contacts', { contacts });
+  res.render('contacts', { 
+    contacts, 
+    contactType: 'famille',
+    ageMin: 0,
+    ageMax: 120  
+  });
 });
 
 router.post('/filter', async (req, res) => {
@@ -65,7 +93,12 @@ router.post('/filter', async (req, res) => {
     contactType: req.body.contactType
   });
 
-  res.render('contacts', { contacts });
+  res.render('contacts', { 
+    contacts, 
+    contactType: req.body.contactType,
+    ageMin: req.body.ageMin,
+    ageMax: req.body.ageMax
+  });
 });
 
 module.exports = router;
